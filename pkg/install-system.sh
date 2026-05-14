b6945a2974b358f3e7ef1f75bd9b36d56db3588f
Gunnl <haevaristo@gmail.com>
2026-05-14 13:50:48 +0200

    Fixed issue in install-system on the disk warning if else statement; added code to partition and format disk for efi and btrfs volumes.

--- pkg/install-system.sh
+++ pkg/install-system.sh
@@ -7,17 +7,40 @@ fi
 
 echo "****************************** installing system"
 echo "******************** partitioning disk"
-read -p "**WARNING** This will erase all your data! Do you wish to continue? (y/n): " response
-if [[ "$response" == "y" || "$response" == "Y" || "$response" == "yes" || "$response" == "Yes"]]
+read -p "*** WARNING *** This will erase all your data! Do you wish to continue? (y/n): " response
+if ! [[ "$response" == "y" || "$response" == "Y" || "$response" == "yes" || "$response" == "Yes" || "$response" == "YES" ]]
 else
 	echo "Aborting!"
 	exit 0
 fi
-
 wipefs -a /dev/nvme0n1
 parted /dev/nvme0n1 --script mklabel gpt
 parted -a optimal /dev/nvme0n1 --script mkpart primary fat32 1 1000MB
 parted /dev/nvme0n1 --script set 1 esp on
 parted /dev/nvme0n1 --script set 1 boot on
-parted -a optimal /dev/nvme0n1 --script mkpart primary linux-swap 1000MB 5000MB
-parted -a optimal /dev/nvme0n1 --script mkpart primary ext4 5000MB 100%
+parted -a optimal /dev/nvme0n1 --script mkpart primary btrfs 1000MB 100%
+
+mkfs.fat -F 32 /dev/nvme0n1p1
+
+crytpsetup luksFormat /dev/nvme0n1p2
+cryptsetup open /dev/nvme0n1p2 root
+
+mkfs.btrfs /dev/mapper/root
+
+mount /dev/mapper/root /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@swap
+
+# unmount partition and mount volumes
+umount /mnt
+mount -o subvol=@ /dev/mapper/root /mnt
+mkdir -p /mnt/home
+mkdir -p /mnt/swap
+mount -o subvol=@home /dev/mapper/root /mnt/home
+mount -o subvol=@swap /dev/mapper/root /mnt/swap
+
+# mount EFI partition
+mkdir -p /mnt/efi
+mount /dev/nvme0p1 /mnt/efi
+
