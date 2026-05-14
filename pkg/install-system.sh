0bcf48263ac82a1059702197f9a80a6329f53bcd
Gunnl <haevaristo@gmail.com>
2026-05-14 13:04:30 +0200

    Added install-system initial partitionation

--- pkg/install-system.sh
+++ pkg/install-system.sh
@@ -5,5 +5,12 @@ if [ "$EUID" -ne 0 ]
 		exit
 fi
 
-echo "*********** installing system"
+echo "****************************** installing system"
+echo "******************** partitioning disk"
+parted /dev/nvme0n1 --script mklabel gpt
+parted -a optimal /dev/nvme0n1 --script mkpart primary fat32 1 1000MB
+parted /dev/nvme0n1 --script set 1 esp on
+parted /dev/nvme0n1 --script set 1 boot on
+parted -a optimal /dev/nvme0n1 --script mkpart primary linux-swap 1000MB 5000MB
+parted -a optimal /dev/nvme0n1 --script mkpart primary ext4 5000MB 100%
 
