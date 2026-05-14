cc6403b80c72b0cb079eb8f1b608ea7e57eedc22
Gunnl <haevaristo@gmail.com>
2026-05-14 13:17:48 +0200

    Added disk wipe and user confirmation to install-system

--- pkg/install-system.sh
+++ pkg/install-system.sh
@@ -7,10 +7,17 @@ fi
 
 echo "****************************** installing system"
 echo "******************** partitioning disk"
+read -p "**WARNING** This will erase all your data! Do you wish to continue? (y/n): " response
+if [[ "$response" == "y" || "$response" == "Y" || "$response" == "yes" || "$response" == "Yes"]]
+else
+	echo "Aborting!"
+	exit 0
+fi
+
+wipefs -a /dev/nvme0n1
 parted /dev/nvme0n1 --script mklabel gpt
 parted -a optimal /dev/nvme0n1 --script mkpart primary fat32 1 1000MB
 parted /dev/nvme0n1 --script set 1 esp on
 parted /dev/nvme0n1 --script set 1 boot on
 parted -a optimal /dev/nvme0n1 --script mkpart primary linux-swap 1000MB 5000MB
 parted -a optimal /dev/nvme0n1 --script mkpart primary ext4 5000MB 100%
-
